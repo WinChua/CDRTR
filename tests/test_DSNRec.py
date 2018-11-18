e7f2ea8585269cbcbaf08cf792a79041559a27c6
winchua <winchua@DLForCCNL.scut.edu.cn>
2018-11-18 22:14:56 +0800

    增加对应的DSNRec测试代码

--- tests/test_DSNRec.py
+++ tests/test_DSNRec.py
@@ -1,9 +1,11 @@
 # coding=utf8
+import pdb
 
 from CDRTR.config import Config
 import unittest
 import tensorflow as tf
 import numpy as np
+from CDRTR.utils import pkdump
 from CDRTR.core.DeepModel.DSNRec import DSNRec
 from CDRTR.dataset import DSNRecDataset
 
@@ -18,8 +20,8 @@ class DSNRecTestSuite(unittest.TestCase):
         item_ipt_shp = 32
         item_ipt = np.random.randint(
             0, 9, size=record_num * item_ipt_shp).reshape((record_num, item_ipt_shp))
-        item_enc_shp = [55, 32]
-        item_dec_shp = [32, 55, item_ipt_shp]
+        item_enc_shp = [25, 9]
+        item_dec_shp = [9, 25, item_ipt_shp]
 
         user_ipt_shp = 32
         usrc_ipt = np.random.randint(
@@ -28,9 +30,9 @@ class DSNRecTestSuite(unittest.TestCase):
             0, 9, size=2*record_num * user_ipt_shp).reshape((2*record_num, user_ipt_shp))
         usrc_rating = np.random.randint(0, 6, size=record_num)
 
-        user_enc_shp = [77, 55, 32]
-        user_dec_shp = [32, 55, 77, user_ipt_shp]
-        user_shr_shp = [77, 55, 32]
+        user_enc_shp = [25, 16, 9]
+        user_dec_shp = [9, 16, 25, user_ipt_shp]
+        user_shr_shp = [25, 16, 9]
         dsn_rec = DSNRec(
             item_ipt_shp, item_enc_shp, item_dec_shp,
             user_ipt_shp, user_enc_shp, user_dec_shp, user_shr_shp)
@@ -45,9 +47,10 @@ class DSNRecTestSuite(unittest.TestCase):
         dataset = DSNRecDataset.DSNRecDataset(
                 "exam/data/preprocess/uirepresent",
                 "exam/data/preprocess/cold",
-                "Auto", "Musi")
-        trainBatchGen = dataset.generateBatch("user", 500)
-        for epoch in range(50):
+                "Musi", "Auto")
+        trainBatchGen = dataset.generateTrainBatch("user", 500)
+        preds = []
+        for epoch in range(500):
             for i in range(100):
                 batchData = next(trainBatchGen)
                 batch = {
@@ -56,18 +59,28 @@ class DSNRecTestSuite(unittest.TestCase):
                     "user_src_rating": batchData["src"]["rating"],
                     "user_tgt_ipt": batchData["tgt"]["user"]
                     }
-                for v in batch.values():
-                    print v.shape
+               # for v in batch.values():
+               #     print v.shape
                 loss = dsn_rec.trainBatch(batch, sess)
-                print "loss of (i, epoch):(%d, %d) is %f" % (i, epoch, loss)
+               # print "loss of (i, epoch):(%d, %d) is %f" % (i, epoch, loss)
+            # pdb.set_trace()
+
+            pred, testloss = dsn_rec.evaluate(sess, dataset.generateTestBatch("user", 1000))
+            print "the loss of test dataset is", testloss
+            preds.append((epoch, pred, testloss))
+
+        pkdump((preds, dataset.usersplit['src']['test']), "test_pred.pk")
 
     def test_DSNRecDataset(self):
         dataset = DSNRecDataset.DSNRecDataset(
             "exam/data/preprocess/uirepresent",
             "exam/data/preprocess/cold",
             "Auto", "Musi")
-        trainBatchGen = dataset.generateBatch("user", 10)
+        trainBatchGen = dataset.generateTrainBatch("user", 10)
+        testBatchGen = dataset.generateTestBatch("user", 10)
         for i in range(10):
             batch = next(trainBatchGen)
-            print batch
+            # print batch
+        for i in range(10):
+            batch = next(testBatchGen)
 
