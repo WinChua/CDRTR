f61cf84c096a124066af90f6536d85be630ecdff
WinChua <winchua@foxmail.com>
2018-12-03 23:12:52 +0800

    修改输出结果脚本

--- exam/show_result.py
+++ exam/show_result.py
@@ -1,8 +1,28 @@
 import pickle
 import math
 import sys
-import os
-with open(sys.argv[1]) as f:
-    data = pickle.load(f)
-domain = sys.argv[1].split("/")[-2].split("_")[0]
-print(domain, "rmse is", math.sqrt(min(data)))
+from prettytable import PrettyTable
+result = PrettyTable(["domain", "rmse"])
+
+
+def get_rmse(result_file):
+    with open(result_file) as f:
+        data = pickle.load(f)
+    domain = result_file.split("/")[-2].split("_")[0]
+    return domain, math.sqrt(min(data))
+
+
+Usage = '''
+<Usage %s test_mse.pk_path ...>
+e.g:
+    python show_result.py `find . -name "test*.pk"`'''
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print Usage % sys.argv[0]
+        sys.exit(1)
+
+    for res in sys.argv[1:]:
+        result.add_row(get_rmse(res))
+
+    print result
